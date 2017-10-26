9ba26b2904fd5a5423287e82309825244ceba3ae
lr <liviu.radulescu@univie.ac.at>
2017-10-26 13:22:35 +0200

    update ChangeLog

--- showphoto.php
+++ showphoto.php
@@ -27,7 +27,7 @@ $st = Utils::stopwatch();
 
 $photo = $_GET['photo'];
 if (!file_exists($photo)) {
-  die("ERROR: Photo not available.\n");
+    die("ERROR: Photo not available.\n");
 }
 
 $ext = "";
@@ -54,46 +54,43 @@ if ($size === 'thumb' && Config::SQUARE_THUMBS) $squared = true;
 
 // if thumbnail size is requested, use configured thumbnail size
 if ($size === 'thumb') {
-  $new_height = Config::THUMBS_SIZE;
-}
-// else, if size is passed, use the values from the array with usual dimenstions (heights)
+    $new_height = Config::THUMBS_SIZE;
+} // else, if size is passed, use the values from the array with usual dimenstions (heights)
 elseif ($size && Config::$DIMS[$size]) {
-  $new_height = Config::$DIMS[$size];
-}
-// else, try to make the best choice from the dimensions to fill the user browser visible area
+    $new_height = Config::$DIMS[$size];
+} // else, try to make the best choice from the dimensions to fill the user browser visible area
 elseif (!$new_height && !$size && $sh) {
-  foreach (Config::$DIMS as $d => $h) {
-    $w = $rap * $h;
-    if ($w <= ($sw - 40) && $h <= ($sh - 60) && $h > $new_height) {
-      $new_height = $h;
-      $size = $d;
+    foreach (Config::$DIMS as $d => $h) {
+        $w = $rap * $h;
+        if ($w <= ($sw - 40) && $h <= ($sh - 60) && $h > $new_height) {
+            $new_height = $h;
+            $size = $d;
+        }
     }
-  }
 }
 // default values in case everything above failed to determine picture height
 if (!$new_height) $new_height = Config::$DIMS["S"];
 
 // do not shrink
 if ($new_height > $height) {
-  $new_height = $height;
-  $size = "O"; // original size
+    $new_height = $height;
+    $size = "O"; // original size
 }
 
 $xoffset = $yoffset = 0;
 
+$new_width = 0;
 if ($squared) {
-  $new_width = $new_height;
-  if ($width > $height) {
-    $xoffset = ceil(($width - $height) / 2);
-    $width = $height;
-  }
-  else {
-    $yoffset = ceil(($height - $width) / 2);
-    $height = $width;
-  }
-}
-elseif ($new_height) {
-  $new_width = round($rap * $new_height);
+    $new_width = $new_height;
+    if ($width > $height) {
+        $xoffset = ceil(($width - $height) / 2);
+        $width = $height;
+    } else {
+        $yoffset = ceil(($height - $width) / 2);
+        $height = $width;
+    }
+} elseif ($new_height) {
+    $new_width = round($rap * $new_height);
 }
 
 $logline = "INFO: $photo (window:{$sw}x{$sh}) - resize to $size ({$width}x{$height} => {$new_width}x{$new_height})";
@@ -101,29 +98,26 @@ $logline = "INFO: $photo (window:{$sw}x{$sh}) - resize to $size ({$width}x{$heig
 $quality = Config::IMG_QUALITY;
 
 if ($size === 'thumb') {
-  $cache_filename = md5('thumb' . $size . $photo . $squared . Config::THUMBS_SIZE);
-}
-else {
-  $cache_filename = md5('photo' . $size . $photo . $quality);
+    $cache_filename = md5('thumb' . $size . $photo . $squared . Config::THUMBS_SIZE);
+} else {
+    $cache_filename = md5('photo' . $size . $photo . $quality);
 }
 $cachedphoto = Config::CACHE_PATH . $cache_filename . '.' . $ext;
 if (file_exists($cachedphoto)) {
-  $logline .= " - CACHE HIT ($cachedphoto)";
-  $photo = $cachedphoto;
-}
-else {
-  if ($size === 'thumb') {
-    $cmd = "convert -verbose \"$photo\" -crop {$width}x{$height}+{$xoffset}+{$yoffset} -thumbnail {$new_height}x{$new_height} \"$cachedphoto\"";
-  }
-  else {
-    $cmd = "convert -verbose \"$photo\" -resize {$new_width}x{$new_height} -bordercolor \"#9999A9\" -border 1 -bordercolor black -border 2 -quality $quality \"$cachedphoto\"";
-  }
-  @exec($cmd, $output, $ret_var);
-  if ($ret_var) {
-    @error_log("ERROR: Resizing image ($photo) failed. Command was: $cmd");
-    die("ERROR: Resizing image ($photo) failed.");
-  }
-  $logline .= " - CACHE CREATED ($cachedphoto)";
+    $logline .= " - CACHE HIT ($cachedphoto)";
+    $photo = $cachedphoto;
+} else {
+    if ($size === 'thumb') {
+        $cmd = "convert -verbose \"$photo\" -crop {$width}x{$height}+{$xoffset}+{$yoffset} -thumbnail {$new_height}x{$new_height} \"$cachedphoto\"";
+    } else {
+        $cmd = "convert -verbose \"$photo\" -resize {$new_width}x{$new_height} -bordercolor \"#9999A9\" -border 1 -bordercolor black -border 2 -quality $quality \"$cachedphoto\"";
+    }
+    @exec($cmd, $output, $ret_var);
+    if ($ret_var) {
+        @error_log("ERROR: Resizing image ($photo) failed. Command was: $cmd");
+        die("ERROR: Resizing image ($photo) failed.");
+    }
+    $logline .= " - CACHE CREATED ($cachedphoto)";
 }
 
 if (preg_match("/(jpg|jpeg)$/i", $photo)) header('Content-type: image/jpeg');
@@ -135,7 +129,6 @@ $fp = fopen($cachedphoto, "rb");
 fpassthru($fp);
 fclose($fp);
 
-$logline .= " - took " .  Utils::stopwatch($st);
+$logline .= " - took " . Utils::stopwatch($st);
 @error_log($logline);
 
-?>
